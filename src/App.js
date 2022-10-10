6fc0b8b77db05c904431a8fa0f4b2bd55c554a54
unknown <mosharafctg111@gmail.com>
2022-10-10 22:47:21 +0600

    added the routerprovider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const router = createBrowserRouter([
+
+  ])
   return (
     <div className="App">
-     
+     <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/topics'>Topics</Link>
+                <Link to='/statistics'>Statistics</Link>
+                <Link to='/blog'>Blog</Link>
+                
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
